0b7629c915977fb58ddc6d02458524d7912bdc92
John Sapp <sappjohn1994@gmail.com>
2024-10-09 00:58:09 -0400

    add "fileDataEntry()" method

--- DMS/src/RocketCollections.java
+++ DMS/src/RocketCollections.java
@@ -2,6 +2,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Scanner;
+import java.io.*;
 
 public class RocketCollections {
     static Scanner dataInput = new Scanner(System.in);
@@ -57,7 +58,7 @@ public class RocketCollections {
                 String l_Date = dataInput.nextLine();
                 inputDateType = LocalDate.parse(l_Date,dateFormat);
             } catch (DateTimeParseException e) {
-                System.out.println("Error with date, make sure the date it formated 'yyyy-MM-dd'");
+                System.out.println("Error with date, make sure the date it formated 'YYYY-MM-DD'");
                 correctEntry = true;
             }
         }
@@ -143,6 +144,7 @@ public class RocketCollections {
         }
         if (optionNumber == 1){
             System.out.println("upload file");
+            fileDataEntry();
         }
         else if (optionNumber == 2){
             System.out.println("Enter Data manually");
@@ -177,16 +179,17 @@ public class RocketCollections {
             String l_ID = dataInput.nextLine();
             int number_ID = Integer.parseInt(l_ID);
 
-            for(RocketDataObject rL : RocketDataObject.launchList){
+            for (int i = 0 ; i < RocketDataObject.launchList.size(); i++){
                 boolean editing = true;
+                boolean correctEntry = true;
                 int userInput = 0;
-                if (number_ID == rL.getLaunch_ID()){
+                if (number_ID == RocketDataObject.launchList.get(i).getLaunch_ID()){
                     switch(optionNumber){
-                        case 1:
+                        case 1: // edit row
                             while(editing){
                                 //prints the current data for the selected launch
-                                System.out.println("\nCurrent Data of this launch\n");
-                                readDataObject(rL);
+                                System.out.println("\nCurrent Data for this launch\n");
+                                readDataObject(RocketDataObject.launchList.get(i));
                                 System.out.println("Select a data point to update or exit");
                                 System.out.println("\t1 - Launch ID");
                                 System.out.println("\t2 - Launch Provider");
@@ -199,47 +202,55 @@ public class RocketCollections {
                                 System.out.println("\t9 - Return to main menu");
 
                                 // gets user selection number
-                                String input = dataInput.nextLine();
-                                try {
-                                    userInput = Integer.parseInt(input);
-                                }catch (NumberFormatException e){
-                                    System.out.println("Please enter only numbers");
-                                    editing = true;
+                                while(correctEntry){
+                                    try {
+                                        correctEntry = false;
+                                        String input = dataInput.nextLine();
+                                        userInput = Integer.parseInt(input);
+                                        if(userInput < 1 || userInput > 9){
+                                            System.out.println("Invalid Input");
+                                            correctEntry = true;
+                                        }
+                                    }catch (NumberFormatException e){
+                                        System.out.println("Please enter only numbers");
+                                        correctEntry = true;
+                                    }
                                 }
+                                correctEntry = true;
                                 switch (userInput){
                                     // calls correct userInput method and sets the object to the new value
                                     case 1:
-                                        rL.setLaunch_ID(launchIDInput());
+                                        RocketDataObject.launchList.get(i).setLaunch_ID(launchIDInput());
+                                        userInput = 0;
                                         break;
-
                                     case 2:
-                                        rL.setLaunch_Provider(launchProviderInput());
+                                        RocketDataObject.launchList.get(i).setLaunch_Provider(launchProviderInput());
+                                        userInput = 0;
                                         break;
-
                                     case 3:
-                                        rL.setLaunch_Location(launchLocationInput());
+                                        RocketDataObject.launchList.get(i).setLaunch_Location(launchLocationInput());
+                                        userInput = 0;
                                         break;
-
                                     case 4:
-                                        rL.setLaunch_Vehicle(launchVehicleInput());
+                                        RocketDataObject.launchList.get(i).setLaunch_Vehicle(launchVehicleInput());
+                                        userInput = 0;
                                         break;
-
                                     case 5:
-                                        rL.setLaunch_date(launchDateInput());
+                                        RocketDataObject.launchList.get(i).setLaunch_date(launchDateInput());
+                                        userInput = 0;
                                         break;
-
                                     case 6:
-                                        rL.setNumber_of_Crew(numberCrewInput());
+                                        RocketDataObject.launchList.get(i).setNumber_of_Crew(numberCrewInput());
+                                        userInput = 0;
                                         break;
-
                                     case 7:
-                                        rL.setPayload(payloadInput());
+                                        RocketDataObject.launchList.get(i).setPayload(payloadInput());
+                                        userInput = 0;
                                         break;
-
                                     case 8:
-                                        rL.setTonnage_to_Orbit(tonnageOrbitInput());
+                                        RocketDataObject.launchList.get(i).setTonnage_to_Orbit(tonnageOrbitInput());
+                                        userInput = 0;
                                         break;
-
                                     case 9:
                                         System.out.println("finished editing this entry");
                                         editing = false;
@@ -247,14 +258,18 @@ public class RocketCollections {
                                 }
                             }
                             break;
-                        case 2:
-                            System.out.println("The data below has been deleted");
-                            readDataObject(rL);
-                            RocketDataObject.launchList.remove(rL);
+                        case 2: // delete row
+                            if(RocketDataObject.launchList.isEmpty()){
+                                System.out.println("No Data to delete");
+                            }else {
+                                System.out.println("The data below has been deleted");
+                                readDataObject(RocketDataObject.launchList.get(i));
+                                RocketDataObject.launchList.remove(RocketDataObject.launchList.get(i));
+                            }
                             break;
                     }
                 }
-                else{
+                else if(i == RocketDataObject.launchList.size()){
                     System.out.println("This Launch ID does not exist");
                 }
             }
@@ -266,7 +281,66 @@ public class RocketCollections {
         for (RocketDataObject rL : RocketDataObject.launchList) {
             totalTonnage += rL.getTonnage_to_Orbit();
         }
-        System.out.println("Total Tonnage: "+totalTonnage+"\n");
+        System.out.println("Total Tonnage to Orbit: "+totalTonnage+"\n");
+    }
+
+    public static void fileDataEntry(){
+        boolean correctFile = true;
+        Scanner readData = null;
+        String dataRow;
+        String[] splitData;
+        int count = 0;
+        int l_ID = 0;
+        String l_Provider = null;
+        String l_Location = null;
+        String l_Vehicle = null;
+        LocalDate l_Date = null;
+        int crew = 0;
+        String payload = null;
+        double tonnage = 0;
+        //RocketDataObject launchData = null;
+
+        System.out.println("Enter the absolute path to your text (.txt) file");
+        //allows the user to reenter path if error
+        while (correctFile) {
+            correctFile = false;
+            String filePath = dataInput.nextLine();
+            File dataFile = new File(filePath);
+            try {
+                readData = new Scanner(dataFile);
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found, try again");
+                correctFile = true;
+            }
+        }
+        while(readData.hasNext()){ // loops through all lines in file
+            dataRow = readData.nextLine(); //reads in the next line from file
+            splitData = dataRow.split(","); // splits and stores strings to array
+            count++;
+
+            try {
+
+                l_ID = Integer.parseInt(splitData[0]);
+                l_Provider = splitData[1];
+                l_Location = splitData[2];
+                l_Vehicle = splitData[3];
+                l_Date = LocalDate.parse(splitData[4]);
+                crew = Integer.parseInt(splitData[5]);
+                payload = splitData[6];
+                tonnage = Double.parseDouble(splitData[7]);
+
+            }catch (NumberFormatException e){
+                System.out.println("Invalid integer input on line: " + count);
+            }catch (DateTimeParseException e){
+                System.out.println("Invalid date input on line: " + count);
+            }
+
+            RocketDataObject launchData = new RocketDataObject
+                    (l_ID,l_Provider,l_Location,l_Vehicle,l_Date,crew,payload,tonnage);
+
+            RocketDataObject.launchList.add(launchData);
+            readDataObject(launchData);
+        }
     }
 
 
